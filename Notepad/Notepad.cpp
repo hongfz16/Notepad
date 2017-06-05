49f4161816fce5954b038e9fb6bf4e1ac960609a
hongfz16@163.com <hongfz16@163.com>
2017-06-05 20:09:11 +0800

    hongfz

--- Notepad/Notepad.cpp
+++ Notepad/Notepad.cpp
@@ -163,6 +163,24 @@ END_MESSAGE_MAP()
 
 void CNotepadApp::OnFont()
 {
+	/*
+	typedef struct tagLOGFONT
+{ LONG lfHeight;
+LONG lfWidth;
+LONG lfEscapement;
+LONG lfOrientation;
+LONG lfWeight;
+BYTE lfItalic;
+BYTE lfUnderline;
+BYTE lfStrikeOut;
+BYTE lfCharSet;
+BYTE lfOutPrecision;
+BYTE lfClipPrecision;
+BYTE lfQuality;
+BYTE lfPitchAndFamily;
+TCHAR lfFaceName[LF_FACESIZE];
+} LOGFONT;
+	*/
 	LOGFONT lf;
 	CFontDialog dlg;
 	COLORERF col;
